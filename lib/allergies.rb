2e948b35debd203a61c395f37715e9d6272eed86
Adam McFadden <mcfadden.113@gmail.com>
2015-04-23 21:47:01 -0700

    make the method work for all allergies within the 1-128 range.

--- lib/allergies.rb
+++ lib/allergies.rb
@@ -1,1 +1,65 @@
 class String
+  define_method(:allergies) do
+    allergy_score = self.to_i
+    allergies_output = ''
+
+    allergy_hash = {128 => 'cats', 64 => 'pollen', 32 => 'chocolate', 16 => 'tomatos', 8 => 'strawberries', 4 => 'shellfish', 2 => 'peanuts', 1 => 'eggs'}
+
+    until allergy_score.to_i < 128
+      allergy_score = allergy_score - 128
+      allergies_output += allergy_hash.fetch(128)
+      if allergy_score > 0
+        allergies_output += ', '
+      end
+    end
+    until allergy_score.to_i < 64
+      allergy_score = allergy_score - 64
+      allergies_output += allergy_hash.fetch(64)
+      if allergy_score > 0
+        allergies_output += ', '
+      end
+    end
+    until allergy_score.to_i < 32
+      allergy_score = allergy_score - 32
+      allergies_output += allergy_hash.fetch(32)
+      if allergy_score > 0
+        allergies_output += ', '
+      end
+    end
+    until allergy_score.to_i < 16
+      allergy_score = allergy_score - 16
+      allergies_output += allergy_hash.fetch(16)
+      if allergy_score > 0
+        allergies_output += ', '
+      end
+    end
+    until allergy_score.to_i < 8
+      allergy_score = allergy_score - 8
+      allergies_output += allergy_hash.fetch(8)
+      if allergy_score > 0
+        allergies_output += ', '
+      end
+    end
+    until allergy_score.to_i < 4
+      allergy_score = allergy_score - 4
+      allergies_output += allergy_hash.fetch(4)
+      if allergy_score > 0
+        allergies_output += ', '
+      end
+    end
+    until allergy_score.to_i < 2
+      allergy_score = allergy_score - 2
+      allergies_output += allergy_hash.fetch(2)
+      if allergy_score > 0
+        allergies_output += ', '
+      end
+    end
+    until allergy_score.to_i < 1
+      allergy_score = allergy_score - 1
+      allergies_output += allergy_hash.fetch(1)
+    end
+
+    allergies_output
+
+  end
+end
--- spec/allergies_spec.rb
+++ spec/allergies_spec.rb
@@ -2,7 +2,10 @@ require 'rspec'
 require 'allergies'
 
 describe('String#allergies') do
-  it('receives an input number and sends out the appropriate allergy') do
-    expect('128'.allergies().to('cats'))  
+  it('receives an input number that should return only one allergy and sends out the appropriate allergy') do
+    expect('128'.allergies()).to(eq('cats'))
+  end
+  it('receives an input number that should return more than one allergy and sends out all appropriate allergies') do
+    expect('172'.allergies()).to(eq('cats, chocolate, strawberries, shellfish'))
   end
 end
